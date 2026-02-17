64ea451e15d2cf231e8306ec59a9eb97477562f4
Aman Gupta <146326514+whoisamanai@users.noreply.github.com>
2026-02-17 18:17:53 +0530

    Change CSS import from index.css to style.css

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
-import './index.css';
+import './style.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
