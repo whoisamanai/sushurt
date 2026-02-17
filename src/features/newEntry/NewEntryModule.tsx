a1fdd06eac134eda13219fae1a87d4fde06f3cae
Aman Gupta <146326514+whoisamanai@users.noreply.github.com>
2026-02-17 18:31:43 +0530

    Fix import path for PrintPreview component

--- src/features/newEntry/NewEntryModule.tsx
+++ src/features/newEntry/NewEntryModule.tsx
@@ -2,7 +2,7 @@ import { FormEvent, useState } from 'react';
 import { Timestamp } from 'firebase/firestore';
 import { addRecord } from '../../services/patientService';
 import { useModuleContext } from '../../context/ModuleContext';
-import { PrintPreview } from '../print/PrintPreview';
+import { PrintPreview } from '../../print/PrintPreview';
 import type { Patient, PatientInput } from '../../types';
 
 const initialState: PatientInput = {
