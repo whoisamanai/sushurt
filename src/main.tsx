6bc8a8deb138fc9e88847573b10a9e8d74ba1c57
Aman Gupta <146326514+whoisamanai@users.noreply.github.com>
2026-02-17 19:02:08 +0530

    Rename style.css to styles.css

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
-import './style.css';
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
